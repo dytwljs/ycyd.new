1c478f6355bd967e11495e3cbd7b990ebf1a4697
dlw1398287387 <15908619974@163.com>
2018-08-03 15:01:47 +0800

    111

--- client/config.js
+++ client/config.js
@@ -4,7 +4,8 @@
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
 // var host = 'https://www.dytwljs.com';
-var host = 'http://localhost:8360';
+// var host = 'http://localhost:8360';
+var host = 'http://192.168.1.106:8360';
 // var host = 'http://192.168.1.8:8360';
 // var host = 'https://f2rhcxzk.qcloud.la';
 //  var host = 'http://192.168.0.191:8360';
